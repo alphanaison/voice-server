8ef4fdfd97e9d9473f078949ad0d15f7d970768b
alphanaison <alphalphanaison@gmail.com>
2026-04-19 15:10:32 +0200

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const WebSocket = require("ws");
 
+console.log("SERVER STARTED");
+
 const server = http.createServer();
 
 const wss = new WebSocket.Server({ server });
@@ -8,25 +10,31 @@ const wss = new WebSocket.Server({ server });
 let clients = [];
 
 wss.on("connection", (ws) => {
+  console.log("CLIENT CONNECTED");
+
   clients.push(ws);
 
   ws.on("message", (message) => {
+    console.log("MESSAGE RECEIVED:", message.toString());
+
     let data;
 
     try {
       data = JSON.parse(message);
     } catch (e) {
+      console.log("INVALID JSON");
       return;
     }
 
     clients.forEach(client => {
-      if (client !== ws && client.readyState === 1) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify(data));
       }
     });
   });
 
   ws.on("close", () => {
+    console.log("CLIENT DISCONNECTED");
     clients = clients.filter(c => c !== ws);
   });
 });

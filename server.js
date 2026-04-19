e708f2c26a8b90efc3570d2bd07fa0d9246b4d48
alphanaison <alphalphanaison@gmail.com>
2026-04-19 18:02:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,56 +4,44 @@ const WebSocket = require("ws");
 console.log("SERVER STARTED");
 
 const server = http.createServer((req, res) => {
-  res.end("WebRTC server alive");
+  res.end("alive");
 });
 
 const wss = new WebSocket.Server({ server });
 
-// store users by ID
+// store users
 const users = new Map();
 
-/*
-Client must first send:
-{ register: "user1" }
-*/
-
 wss.on("connection", (ws) => {
-  let userId = null;
+  let id = null;
 
   ws.on("message", (msg) => {
     let data;
-
     try {
       data = JSON.parse(msg.toString());
     } catch {
       return;
     }
 
-    // REGISTER USER
-    if (data.register) {
-      userId = data.register;
-      users.set(userId, ws);
-      console.log("REGISTERED:", userId);
+    // REGISTER
+    if (data.type === "register") {
+      id = data.id;
+      users.set(id, ws);
+      console.log("REGISTERED:", id);
       return;
     }
 
-    // ROUTE CALLS
+    // ROUTE MESSAGE
     if (data.to && users.has(data.to)) {
-      const target = users.get(data.to);
-
-      if (target.readyState === WebSocket.OPEN) {
-        target.send(JSON.stringify(data));
-      }
+      users.get(data.to).send(JSON.stringify(data));
     }
   });
 
   ws.on("close", () => {
-    if (userId) users.delete(userId);
+    if (id) users.delete(id);
   });
 });
 
-const PORT = process.env.PORT || 10000;
-
-server.listen(PORT, "0.0.0.0", () => {
-  console.log("Listening on", PORT);
+server.listen(process.env.PORT || 10000, "0.0.0.0", () => {
+  console.log("Listening");
 });
